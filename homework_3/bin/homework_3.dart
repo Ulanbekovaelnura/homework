d59dce264cadf6708481eb409093f42d89db19f9
Ulanbekovaelnura <115091221+Ulanbekovaelnura@users.noreply.github.com>
2022-12-08 12:03:50 +0600

    dart_appication_2

--- homework_3/bin/homework_3.dart
+++ homework_3/bin/homework_3.dart
@@ -1,5 +1,48 @@
-import 'package:homework_3/homework_3.dart' as homework_3;
+import 'package:test/expect.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${homework_3.calculate()}!');
+void main() {
+ List ints =  [1, 7, 12, 3, 56, 2, 87, 34, 54];
+    print(ints.first);
+    print(ints[4]);
+    print(ints.last);
+
+
+ List a = [3, 12, 43, 1, 25, 6, 5, 7] ;
+ List b =  [55, 11, 23, 56, 78, 1, 9];
+    print(a + b);
+
+
+ List ints1 =  ['a','d','F','l','u','t','t','e','R','y','3','b','h','j'];
+ ints1.getRange(2, 9);
+    print(ints1.getRange(2, 9));
+
+
+ List intsI =  [1, 2, 3, 4, 5, 6, 7];
+    print(intsI.first);
+    print(intsI.last);
+    print(intsI.length);
+    print(contains(3));
+
+
+ List intsA = [601, 123, 2, "dart", 45, 95, "dart24", 1];
+    print(intsA.contains("dart"));
+    print(intsA.contains(951));
+
+
+ List intsB = ['post', 1, 0, 'flutter'];
+ String myDart = 'Flutter';
+     print(intsB.contains(myDart.toLowerCase()));
+
+
+ List intsV = ["I", "Started", "Learn", "Flutter", "Since", "April"];
+ String myFlutter;
+ myFlutter = intsV.join(' * ');
+    print(myFlutter);
+ 
+
+
+ List intsR =  [1, 9, 3, 195, 202, 2, 5, 7, 9, 10, 3, 15, 0, 11];
+ intsR.sort();
+    print(intsR);
+
 }
